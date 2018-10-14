507cee5a3776e581f6161219ee0af96e972661bc
Bohdan Shurko <shurkobohdan@gmail>
2018-10-14 23:41:08 +0300

    initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int [] numbers = new int[10];
+		
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = i*12;
+			
+		}
+		for(int i=0; i<numbers.length;i++){
+			System.out.println(numbers[i]);
+		}
+		System.out.println("Quite a change");
+	}
+}
